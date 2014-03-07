dd33049814d8b09e2c11f756a5b579ca87dcfeb9
Izzy Park <isy012@hotmail.com>
2014-03-07 01:29:13 -0500

    adding db connect code

--- web.js
+++ web.js
@@ -3,6 +3,17 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+//postgresql
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM userlist', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
+
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
