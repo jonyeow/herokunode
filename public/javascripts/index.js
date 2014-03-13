af37857ba5df5cd3fdde4d11d38ef7c4d2eacbe8
Izzy Park <isy012@hotmail.com>
2014-03-13 16:22:31 -0400

    finished whartoninstagrams

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,67 +1,130 @@
 $(document).ready(function(){
 
-var coords = new Array();
-var latitudes = new Array();
-var longitudes = new Array();
-var locationnames = new Array();
+var map;
+pictureURLS = new Array();
+entireData = new Array();
+var infowindow = null;
 
-//Google Map Coords Philly
-var center = "39.952646,-75.179513&";
-var zoomsize = "zoom=1&";
-var mapsize = "size=600x375&";
-var type="maptype=roadmap&";
-var markers = "markers=color:red%7C";
-var mycoords1 = "39.952646,-75.179513&";
-var mycoords2 = "39.962646,-75.179513&";
-var sensor = "sensor=false";
-var coordinates = "";
+function initialize() {
+
+    //Coordinates of Philadelphia
+    var myLatlng = new google.maps.LatLng(39.952646,-75.179513);
+    var mapOptions = {
+      zoom: 2,
+      center: myLatlng
+      };
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    for (var i = 0; i < entireData.length; i++) {
+
+      var image = {
+      url: entireData[i].user.profile_picture,
+      scaledSize: new google.maps.Size(40, 40), // size
+      origin: new google.maps.Point(0, 0), // origin
+      anchor: new google.maps.Point(20, 20), // anchor 
+      };
+
+      var coords = entireData[i].location;
+      var myLatLng = new google.maps.LatLng(coords.latitude, coords.longitude);
+      var marker = new google.maps.Marker({
+      position: myLatLng,
+      icon: image,
+      animation: google.maps.Animation.DROP,
+      map: map,
+      });
+
+      infowindow = new google.maps.InfoWindow({
+      //content: "<img height=50px width=50px src=" + entireData[i].images.thumbnail.url + "></img><br>" + entireData[0].caption.text
+      content: "holding...",
+      maxWidth: 300 
+      });
+      var content = "<caption><img height=75px width=75px src=" + 
+      entireData[i].images.thumbnail.url + "></img><br>" + entireData[i].caption.text + "</caption>"
+      
+      bindInfoWindow(marker, map, infowindow, content);
+
+      //google.maps.event.addListener(marker, 'click', function () {
+        //console.log("in");
+        //infowindow.setContent(this.html);
+        //infowindow.open(map, marker);
+      //});
+    }
+}
+
+function bindInfoWindow(marker, map, infowindow, strDescription) {
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(strDescription);
+        infowindow.open(map, marker);
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
+});
+
+
+function setInfoWindow(map, marker){
+
+    //var contentString = '<img height=50px width=50px src=' + entireData[i].images.thumbnail.url + '></img><br>'+entireData[0].caption.text;
+    
+    var contentString = 'hi';
+    //set the infowindow
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 300     
+      });
+
+    //var coords = entireData[i].location;
+    //var myLatLng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    var position = marker.position;
+    console.log(marker.position);
+
+    };
+function setMarkers(map, entireData){
+
+  for (var i = 0; i < entireData.length; i++) {
+
+    var image = {
+      url: entireData[i].user.profile_picture,
+      scaledSize: new google.maps.Size(30, 30), // size
+      origin: new google.maps.Point(0, 0), // origin
+      anchor: new google.maps.Point(15, 15), // anchor 
+    };
+
+    var coords = entireData[i].location;
+    var myLatLng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    marker = new google.maps.Marker({
+        position: myLatLng,
+        icon: image,
+        animation: google.maps.Animation.DROP,
+        map: map,
+    });
+  }
+ };
+
+//API Call
 $.ajax({
     type: "GET",
     dataType: "jsonp",
     cache: false,
     url: "https://api.instagram.com/v1/tags/mywharton/media/recent?client_id=7c7762e8edbd461d85365b7eeb9a16b1",
     success: function(data) {
-        for (var i = 0; i < 18; i++) {
+        //entireData = data;
+        for (var i = 0; i < 20; i++) {
             $("#pics").append(
-              //"<img class=img-circle width=40px src='" + data.data[i].user.profile_picture + "'></img>" 
-                //+ "<small>" + data.data[i].user.username + "</small>" 
-               //"<img height=100px width=100px data-lightbox=instagram src='" + data.data[i].images.thumbnail.url + "'></img>"
-               "<a href='" +  data.data[i].images.standard_resolution.url + "' data-lightbox='instagram'><img height=100px width=100px data-lightbox=instagram src='" + data.data[i].images.thumbnail.url + "'></img></a>"
-                );
-            if (data.data[i].location != null){
-                  latitudes.push(data.data[i].location.latitude);
-                  longitudes.push(data.data[i].location.longitude);
-               if (data.data[i].caption.text != null){
-                   locationnames.push(data.data[i].caption.text);
-               }
-            }
-        }
-        for (i=0; i<latitudes.length; i++){
-        coords.push(markers + latitudes[i] + "," + longitudes[i]);
-        //console.log(coords[i]);
-        coordinates += coords[i] + "&";
-        //console.log(coordinates);
-        }
+               "<a href='" +  
+               data.data[i].images.standard_resolution.url + 
+               "' data-lightbox='instagram'><img height=100px width=100px data-lightbox=instagram src='" + 
+               data.data[i].images.thumbnail.url + 
+               "'></img></a>"
+              );
 
-        for (i=0; i<5; i++){
-          $("#locations").append("<li>"+locationnames[i])+"</li>";
+            if (data.data[i].location!=null){
+              entireData.push(data.data[i]);
+            };
         }
-
-        var url = "http://maps.googleapis.com/maps/api/staticmap?center=" + center + zoomsize + mapsize + type + coordinates + sensor;
-        //console.log(url);
-        $("map").after("<img src='" + url + "'></img>");
-
     }
 });
 
-   //var url = "http://maps.googleapis.com/maps/api/staticmap?center=" + center + zoomsize + mapsize + type + markers + mycoords1 +markers + mycoords2 + sensor;
-
-
-  //$("map").html("<img src='" + url + "'></img>");
-
-   
-
-
-});
